37abcf45c3c2124e9d4fae3e9476d48616aad8b3
yuxiangqingfeng <1370976540@qq.com>
2026-09-16 18:07:51 +0800

    1

---
+++ assets/BusinessNote.vue_vue_type_script_setup_true_lang-C8H3X3bO.js
@@ -0,0 +1,1 @@
+import{B as t}from"./BusinessNoteMarker-BLBp4R_3.js";import{G as n,O as o,L as i}from"./vue-core-BtVRMQd0.js";const l=n({__name:"BusinessNote",props:{index:{},title:{},content:{}},setup(e){return(s,c)=>(i(),o(t,{index:e.index,title:e.title,content:e.content},null,8,["index","title","content"]))}});export{l as _};
