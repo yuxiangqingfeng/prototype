25f8de72d0e890b4ec5e9aae817820ea63c47701
yuxiangqingfeng <1370976540@qq.com>
2026-09-16 12:14:22 +0800

    update

---
+++ assets/BusinessNote.vue_vue_type_script_setup_true_lang-Cggo3D8p.js
@@ -0,0 +1,1 @@
+import{B as t}from"./BusinessNoteMarker-DguhAo4x.js";import{G as n,O as o,L as i}from"./vue-core-Bc8znYh5.js";const l=n({__name:"BusinessNote",props:{index:{},title:{},content:{}},setup(e){return(s,c)=>(i(),o(t,{index:e.index,title:e.title,content:e.content},null,8,["index","title","content"]))}});export{l as _};
---
+++ assets/catalogRelations-DG6h-yAY.js
@@ -0,0 +1,1 @@
+function e(r,o){return r.filter(n=>n.songId===o)}function s(r,o){return r.filter(n=>n.copyrightVersionId===o)}export{s as r,e as v};
